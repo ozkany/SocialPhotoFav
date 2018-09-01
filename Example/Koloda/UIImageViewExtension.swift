4639adfdab46c1c45fad9b7f29b0acde2c06f714
Özkan Yilmaz <ozkanyilmaz@gmail.com>
2018-09-01 19:57:39 +0300

    Works with hardcoded image url list array

---
+++ Example/Koloda/UIImageViewExtension.swift
@@ -0,0 +1,31 @@
+//
+//  UIImageViewExtension.swift
+//  Koloda_Example
+//
+//  Created by Ozkan Yilmaz on 1.09.2018.
+//  Copyright © 2018 CocoaPods. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIImageView {
+    func downloaded(from url: URL, contentMode mode: UIViewContentMode = .scaleAspectFit) {
+        contentMode = mode
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            guard
+                let httpURLResponse = response as? HTTPURLResponse, httpURLResponse.statusCode == 200,
+                let mimeType = response?.mimeType, mimeType.hasPrefix("image"),
+                let data = data, error == nil,
+                let image = UIImage(data: data)
+                else { return }
+            DispatchQueue.main.async() {
+                self.image = image
+            }
+            }.resume()
+    }
+    func downloaded(from link: String, mode: UIViewContentMode = .scaleAspectFit) {
+        guard let url = URL(string: link) else { return }
+        downloaded(from: url, contentMode: mode)
+    }
+}
